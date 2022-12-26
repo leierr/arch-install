9d3e5cbbd5d603503f80a3af620e60ead160670f
Lars Smith Eier <lars.smith.eier@basefarm-orange.com>
2022-12-26 01:26:59 +0100

    test

--- install.sh
+++ install.sh
@@ -61,12 +61,7 @@ function partitioning() {
 	wipefs --force --all $install_disk &> /dev/null
 
 	echo -n "-> partition disk: "
-	sfdisk {{ install_drive }} << EOF
-label: gpt
-;512Mib;U;*
-;512Mib;BC13C2FF-59E6-4262-A352-B275FD6F7172
-;+;L
-EOF # && echo -e "\e[32mOK\e[0m"
+	sfdisk $install_disk << EOF label: gpt ;512Mib;U;* ;512Mib;BC13C2FF-59E6-4262-A352-B275FD6F7172 ;+;L EOF
 }
 
 pre_checks
